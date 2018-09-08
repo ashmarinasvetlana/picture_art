b8979abf6753e3c87c88d2f8a30b5a0226fac276
Svetlana <sionova2@gmail.com>
2018-09-08 23:34:27 +0300

    Калькулятор

--- js/calc.js
+++ js/calc.js
@@ -7,16 +7,23 @@ sizeSum = 0;
 materialSum = 0,
 	optionsSum = 0,
 	total = 0;
-
+function isPromo(){
+	if (promo.value == 'IWANTPOPART') { 
+		total = total - (total * 0.3);
+totalValue.innerHTML = total; 
+}
+	
+;}
 size.addEventListener('change', function () {
 
 	sizeSum = +this.options[this.selectedIndex].value;
 	total = sizeSum + materialSum + optionsSum;
-	if (size.value === 'Выберите размер картины' || material.value === 'Выберите материал картины') {
+	if (size.value === '0' || material.value === '0') {
 		totalValue.innerHTML = 'Для расчета нужно выбрать размер картины и материал картины';
 	} else 
 if (promo.value == 'IWANTPOPART') { 
 totalValue.innerHTML = total - (total * 0.3); 
+
 } else { 
 totalValue.innerHTML = total; 
 } 
@@ -28,7 +35,7 @@ material.addEventListener('change', function () {
 
 	materialSum = +this.options[this.selectedIndex].value;
 	total = sizeSum + materialSum + optionsSum;
-	if (size.value === 'Выберите размер картины' || material.value === 'Выберите материал картины') {
+	if (size.value === '0' || material.value === '0') {
 		totalValue.innerHTML = 'Для расчета нужно выбрать размер картины и материал картины';
 	} else 
 if (promo.value == 'IWANTPOPART') { 
@@ -44,7 +51,7 @@ options.addEventListener('change', function () {
 
 	optionsSum = +this.options[this.selectedIndex].value;
 	total = sizeSum + materialSum + optionsSum;
-	if (size.value === 'Выберите размер картины' || material.value === 'Выберите материал картины') {
+	if (size.value === '0' || material.value === '0') {
 		totalValue.innerHTML = 'Для расчета нужно выбрать размер картины и материал картины';
 
 	} else 
@@ -55,4 +62,12 @@ totalValue.innerHTML = total;
 } 
 
 });
+//отслеживаем ввод промокода
+promo.addEventListener('input', () => {
+	if (promo.value == 'IWANTPOPART') { 
+totalValue.innerHTML = total - (total * 0.3); 
+//исключаем повторное использование промокода
+promo.disabled = true;
+} 
 
+});
